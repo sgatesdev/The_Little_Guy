dca9c26df31ded82e074edb19ac655355629edae
zchalk <you@example.com>
2021-06-30 19:52:56 -0400

    messages page added, message list created

---
+++ client/src/pages/Messages.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+export const Messages = () => {
+    // socketio
+    const state = useSelector((state) => state);
+    return (
+        <>
+        <h1><h1 class="uk-heading-small uk-flex uk-flex-center">{ state.user ? `${state.user.username}, you have socketio messages!` : null }</h1></h1>
+        <div className="uk-flex uk-flex-center uk-width-1-2 uk-margin-top">
+            <MessageList/>
+        </div>
+        </>
+
+    )
+}
