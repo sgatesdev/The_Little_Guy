3a428e1f61349203ff7504c1f204dff5750b70b6
Juawito <santiagojuan311@gmail.com>
2021-06-30 12:57:29 -0400

    creating authfile and linking it inside of server file

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas/index');
-// const { authMiddleware } = require('./utils/auth');
+const { AuthMiddleware } = require('./utils/auth');
 const db = require('./config/config');
 
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,7 @@ const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // context: authMiddleware
+    context: authMiddleware
 });
 
 server.applyMiddleware({ app });
---
+++ api/utils/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secret = process.enc.TOKEN_SECRET;
+const experitation = '2h';
+
+module.exports = {
+    AuthMiddleware: async function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) return req;
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: experitation });
+            req.user = data;
+        } catch (error) {
+            console.log('Invalid Token');
+        }
+        return req;
+    },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: experitation })
+    }
+};
