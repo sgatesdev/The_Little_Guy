f8d6c5771581dc5f688d1910166a89a5331502ae
Juawito <santiagojuan311@gmail.com>
2021-06-30 20:49:22 -0400

    creating more resolvers and typedefs

--- api/schemas/resolvers.js
+++ api/schemas/resolvers.js
@@ -1,19 +1,72 @@
-const { AuthenticationError} = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Property } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        user: async () => {
-            return await User.findByPk(1);
+        user: async (id) => {
+            return await User.findByPk(id);
         },
         property: async (parent, { address }) => {
             const params = {};
-            if(address) {
+            if (address) {
                 params.address = address;
             }
             return await Property.find(params);
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findByPk(context.user._id).populate('Property').populate({
+                    path: 'Property.Images',
+                    populate: 'Images'
+                })
+                return user;
+            }
+            throw new AuthenticationError('Not Logged In');
+        },
+        myProperties: async (parent, args, context) => {
+            if (context.user) {
+                const userProperties = await Property.find({ owner: context.user._id }).populate('Images')
+                return userProperties
+            }
+            throw new AuthenticationError('Not logged In!');
+        },
+        tenants: async (parent, args, context) => {
+            if (context.user) {
+                const userTenants = Property.find({ owner: context.user._id }).populate({
+                    path: 'Property.tenant',
+                    populate: 'User'
+                });
+                return userTenats;
+            }
+            throw new AuthenticationError('Not Logged In!');
         }
     },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect Credentials');
+            };
+
+            const passCheck = await user.isCorrectPassword(password);
+
+            if (!passCheck) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        signUp: async (parent, { input }) => {
+            const newUser = await User.create(input);
+            const token = await signToken(user);
+
+            return { token, user };
+        }
+    }
 };
 
 module.exports = resolvers;
--- api/schemas/typeDefs.js
+++ api/schemas/typeDefs.js
@@ -7,14 +7,32 @@ const typeDefs = gql`
     }
 
     type Property {
-        _id: ID,
-        address: String,
+        _id: ID
+        address: String
+    }
+    type Auth {
+        token: ID
+        user: User
     }
 
     type Query {
-        user(_id:ID!): User,
-        me:User,
-        property(address: String): Property, 
+        user(_id:ID!): User
+        me:User
+        property(address: String): Property
+        myProperties: Property
+        tenants(_id: ID!): Property
+    }
+    input NewUserInput {
+        firstName: String
+        lastName: String
+        email: String
+        password: String
+    }
+
+    type Mutation {
+        login(email:String!, password:String!): Auth
+        signUp(input: NewUserInput): Auth
+
     }
 
 `;
