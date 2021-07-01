d0a3b7e5e75b523e5e77a89a6e46802c754b13c2
Juawito <santiagojuan311@gmail.com>
2021-06-30 20:46:21 -0400

    adding seed file and seed data

--- api/config/seeder.js
+++ api/config/seeder.js
@@ -1,1 +1,25 @@
-// for seed file. needs to be created to start testing queries and mutations
+const db = require('./config');
+const { User, Property, Image } = require('../models')
+const userData = require('./users.json');
+const propertyData = require('./properties.json')
+
+db.once('open', async () => {
+    await User.deleteMany();
+
+    const allUsers = await User.insertMany(userData);
+    console.log('Users inserted! :)');
+
+    await Property.deleteMany();
+
+    for (let i = 0; i < allUsers.length; i++) {
+        const user = allUsers[i];
+        propertyData[i].owner = user._id;
+        propertyData[10].tenant = user._id;
+        propertyData[11].tenant = user._id;
+        propertyData[12].tenant = user._id;
+    }
+    await Property.insertMany(propertyData);
+    console.log('Properties Inserted! :)');
+
+    process.exit();
+})
