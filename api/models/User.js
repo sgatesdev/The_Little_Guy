0240df8889b706bd7cf6ccbf05845b2692d3ecbe
Juawito <santiagojuan311@gmail.com>
2021-06-30 21:56:40 -0400

    committing before pulling

--- api/models/User.js
+++ api/models/User.js
@@ -70,6 +70,7 @@ userSchema.pre('save', async function (next) {
     next();
   });
   
+  
   userSchema.methods.isCorrectPassword = async function (password) {
     return bcrypt.compare(password, this.password);
   };
