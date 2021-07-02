fb44807be8e2ff5f905700ce7700270b86aab30f
Juawito <santiagojuan311@gmail.com>
2021-07-02 19:43:30 -0400

    changing path for heroku

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build' )))
 }
 app.get('/', (req, res ) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+    res.sendFile(path.join(publicPath, 'index.html'))
 });
 app.post('/api/uploadImage', async (req, res) => {
     try{
