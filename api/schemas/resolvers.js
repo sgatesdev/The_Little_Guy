89ec380083b055fc9b16bd295654a2a4fdfba7a5
Juawito <santiagojuan311@gmail.com>
2021-07-05 22:06:51 -0400

    creating resolvers for finding landlord and tenants, and working on rating query and updating properties

--- api/schemas/resolvers.js
+++ api/schemas/resolvers.js
@@ -18,12 +18,17 @@ const resolvers = {
         user: async (id) => {
             return await User.findOne({ _id: id });
         },
-        property: async (parent, { address }) => {
-            const params = {};
-            if (address) {
-                params.address = address;
-            }
-            return await Property.find(params);
+        property: async (parent, { input }) => {
+            const property = await Property.findOne(
+                {
+                    addressStreet: input.addressStreet,
+                    addressCity: input.addressCity,
+                    addressState: input.addressState,
+                    addressZip: input.addressZip,
+                   }
+            ).populate('User');
+            if(!property) throw new AuthenticationError('No property found')
+            return property
         },
         me: async (parent, args, context) => {
             if (context.user) {
@@ -37,18 +42,20 @@ const resolvers = {
         },
         myProperties: async (parent, args, context) => {
             if (context.user) {
-                const userProperties = await Property.find({ owner: context.user._id }).populate('Images')
+                const userProperties = await Property.find({ owner: "60dd1d3d495bb208abc6ed0b"}).populate('Images')
                 return userProperties
             }
             throw new AuthenticationError('Not logged In!');
         },
         myTenants: async (parent, args, context) => {
             if (context.user) {
-                const userTenants = Property.find({ owner: context.user._id }).populate({
-                    path: 'Property.tenant',
+                const userTenants = await Property.find({ owner: context.user._id}).populate({
+                    path: 'tenant',
                     populate: 'User'
                 });
-                return userTenats;
+                let tenants = [];
+                userTenants.forEach((property) => tenants.push(property.tenant) )
+                return tenants;
             }
             throw new AuthenticationError('Not Logged In!');
         },
@@ -59,7 +66,41 @@ const resolvers = {
             } catch (error) {
                 throw new AuthenticationError('No Properties found');
             }
-        }
+        },
+        getRating: async (parent, { id }) => {
+            try {
+                const userRating = await User.aggregate([{ $match: { _id: id } },
+                {
+                    $group: {
+                        _id: id,
+                        rating: { $avg: 'rating' }
+                    }
+                }]);
+                console.log(userRating)
+                return userRating
+            } catch (error) {
+                console.log(error)
+                throw new AuthenticationError('There are no rating yet!')
+            }
+        },
+        findLandlord: async (parent, { input }) => {
+            try {
+                const property = await Property.findOne(
+                    {
+                     addressStreet: input.addressStreet,
+                     addressCity: input.addressCity,
+                     addressState: input.addressState,
+                     addressZip: input.addressZip,
+                    }).populate({
+                    path: 'owner',
+                    populate: 'User'
+                });
+                const {owner} = property;
+                return owner
+            } catch (error) {
+                throw new AuthenticationError('No property found')
+            }
+        },
 
     },
 
@@ -91,7 +132,7 @@ const resolvers = {
             //}
 
             const checkEmail = await User.findOne({ email });
-            if(checkEmail) {
+            if (checkEmail) {
                 throw new ApolloError('Email already exists!');
             }
 
@@ -102,11 +143,40 @@ const resolvers = {
             return { token, user };
         },
         updateMyProperties: async (parent, args, context) => {
-            const property = await Property.updateOne({ owner: context.user._id },
-                {}
-            )
+            if(context.user) {
+            const property = await Property.updateOne({ owner: context.user._id })
+            }
+            throw new AuthenticationError('Not Logged In');
+        },
+        updateMySavedProperties: async (parent, context) => {
 
+        },
+        deleteProperty: async (parent, {_id}, context) => {
+            try {
+                if(context.user){
+                const user = await User.findOneDelete({owned_properties: context.user._id});
+                const property = await Property.deleteOne({_id:_id});
+
+                return property;
+                } 
+                throw new AuthenticationError('Not Logged In')
+            } catch (error) {
+                throw new AuthenticationError('No property was found');
+            }
+        },
+        deleteUser: async (parent, context) => {
+            try {
+                if(context.user) {
+                const  user = await User.findByIdAndDelete(context.user._id);
+                const token = ''
+                return { token, user}
+                } 
+                throw new AuthenticationError('Not Logged In')
+            } catch (error) {
+                throw new AuthenticationError(error);
+            }
         }
+
     }
 };
 
