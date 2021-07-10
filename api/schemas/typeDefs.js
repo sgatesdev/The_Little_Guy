c1a13b9ad360d7f322763817104da0d6990a9ca5
Juawito <santiagojuan311@gmail.com>
2021-07-09 22:31:06 -0400

    changing typedef user to single user

--- api/schemas/typeDefs.js
+++ api/schemas/typeDefs.js
@@ -58,7 +58,7 @@ const typeDefs = gql`
         is_landlord: Boolean
     }
     type Query {
-        user(id:ID!): User
+        singleUser(id:ID!): User
         me:User
         property(input: UpdatePropertyInput!): Property
         myProperties: [Property]
