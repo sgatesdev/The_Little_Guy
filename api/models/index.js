dfce9b37b05dd3898d06f3ebf03029548ca1616b
Juawito <santiagojuan311@gmail.com>
2021-07-14 20:21:50 -0400

    addind application to the object containing all models

--- api/models/index.js
+++ api/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Property = require('./Property');
+const Application = require('./Application');
 
-module.exports = { User, Property };
+module.exports = { User, Property, Application };
