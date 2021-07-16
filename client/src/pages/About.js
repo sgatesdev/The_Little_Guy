eb69dcfdcbb2df115d5984af5867a843cb908fe2
zchalk <you@example.com>
2021-07-16 16:40:49 -0400

    added sam to about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -31,8 +31,8 @@ const About = () => {
     <div class="flex items-center md:flex-row justify-evenly">
         <div class="p-4">
             <div class="text-center mb-4 opacity-90">
-            <a href="#" class="block relative">
-                    <img alt="profil" src="/images/person/1.jpg" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+            <a href="https://samgates.io/" class="block relative">
+                    <img alt="samProfile" src={`${process.env.PUBLIC_URL}/assets/headshot-sam.jpeg`} class="mx-auto object-cover rounded-full h-40 w-40 "/>
             </a>
             </div>
             <div class="text-center">
@@ -50,7 +50,7 @@ const About = () => {
                         </path>
                     </svg>
                 </a>
-                <a href="#">
+                <a href="https://www.linkedin.com/in/sam-gates-web-dev/">
                     <svg width="30" height="30" fill="currentColor" class="text-xl hover:text-TLGOrange" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                         <path d="M477 625v991h-330v-991h330zm21-306q1 73-50.5 122t-135.5 49h-2q-82 0-132-49t-50-122q0-74 51.5-122.5t134.5-48.5 133 48.5 51 122.5zm1166 729v568h-329v-530q0-105-40.5-164.5t-126.5-59.5q-63 0-105.5 34.5t-63.5 85.5q-11 30-11 81v553h-329q2-399 2-647t-1-296l-1-48h329v144h-2q20-32 41-56t56.5-52 87-43.5 114.5-15.5q171 0 275 113.5t104 332.5z">
                         </path>
